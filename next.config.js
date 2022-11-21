11f7f201c933401f6aa393ad4b333f3b7fa9079d
Himanshu Yadav <himanshu.yadav@aknamed.com>
2022-11-21 12:59:36 +0530

    Backend url

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     env: {
-      "baseURL": process.env.NODE_ENV === "development" ? "http://localhost:5000": "https://www.api.example.com"
+      "baseURL": process.env.NODE_ENV === "development" ? "http://localhost:5000": "https://coding-blog-backend.onrender.com"
     }
 }
